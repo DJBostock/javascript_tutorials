f877ddc92cede3f14948f0373ff5cd6f699c1fc8
DJBostock <douglasjbostock@gmail.com>
2022-07-19 21:16:15 -0500

    added JS functionality to project

--- course_javascript_web_projects/quote_generator/script.js
+++ course_javascript_web_projects/quote_generator/script.js
@@ -1,1 +1,27 @@
-console.log('testing');
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+    choice = Math.floor(Math.random() * apiQuotes.length);
+    currentQuote = apiQuotes[choice];
+    document.getElementById('quote').innerText = currentQuote.text;
+    document.getElementById('author').innerText = currentQuote.author;
+
+}
+
+// Get Quotes From API
+async function getQuotes() {
+    const apiUrl = "https://type.fit/api/quotes";
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getQuotes();
+
+document.getElementById('new-quote').addEventListener('click', newQuote);
