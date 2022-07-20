02c647edf1661eddcf34a2046bc346a8c6c2e300
DJBostock <douglasjbostock@gmail.com>
2022-07-20 18:43:36 -0500

    refactor a bit and add twitter button functionality

--- course_javascript_web_projects/quote_generator/script.js
+++ course_javascript_web_projects/quote_generator/script.js
@@ -1,12 +1,30 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const quoteAuthor = document.getElementById('author');
+const newQuoteBtn = document.getElementById('new-quote');
+const twitterBtn = document.getElementById('twitter');
+
+// https://twitter.com/intent/tweet
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
     choice = Math.floor(Math.random() * apiQuotes.length);
     currentQuote = apiQuotes[choice];
-    document.getElementById('quote').innerText = currentQuote.text;
-    document.getElementById('author').innerText = currentQuote.author;
+    quoteText.innerText = currentQuote.text;
 
+    if (currentQuote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+
+    if (currentQuote.author == '') {
+        quoteAuthor.innerText = 'Unknown';
+    } else {
+        quoteAuthor.innerText = currentQuote.author;
+    }
 }
 
 // Get Quotes From API
@@ -24,4 +42,11 @@ async function getQuotes() {
 // On Load
 getQuotes();
 
-document.getElementById('new-quote').addEventListener('click', newQuote);
+newQuoteBtn.addEventListener('click', newQuote);
+
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${quoteAuthor.innerText}`;
+    window.open(twitterUrl, '_blank');
+}
+
+twitterBtn.addEventListener('click', tweetQuote);
